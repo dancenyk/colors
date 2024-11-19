eb7ba44730edefce69956fb3b4995dc08ab3b5ac
Danceny Karla <dancenykarla@gmail.com>
2024-11-19 10:20:14 +0100

    version01

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,45 @@
+import { useState, useRef, useEffect } from 'react';
 import './App.css'
 
 function App() {
+  const inputRef = useRef(null)
+  const [input, setInput] = useState("")
+  const colors = ['red', 'green', 'pink', 'yellow', 'purple', 'white', 'blue', 'aqua', 'olive'];
+
+
+
+  useEffect(()=>{
+    inputRef.current.focus()
+  }, [])
+  
+  const handleChange = () =>{
+    setInput(inputRef.current.value)
+  }
+
+  const boxStyle = (color) =>{
+    return color === input ? color :""
+    
+  }
+
+  const changeText = (color) =>{
+    const text = color  === input ? ` Soy  el color  ${input} `: `No soy el color ${input}`
+    return <span style={{color: color === input ? "black" : color}}>{text}</span>
+
+  }
+
   return (
     <>
+    <input 
+    type="text"
+    placeholder='escribe un color'
+    value={input}
+    onChange={handleChange}
+    ref={inputRef}
+    />
+
+    <div className='container'>
+      {colors.map(color => <div style ={{borderColor: color, backgroundColor: boxStyle(color)}}className="box" key={color} >{changeText(color)}</div> )}
+    </div>
     </>
   )
 }
--- src/components/MyFormChallenge.jsx
+++ src/components/MyFormChallenge.jsx
@@ -8,3 +8,5 @@ function MyForm() {
 }
 
 export default MyForm;
+
+
